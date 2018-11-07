42c81237b6dc15718d27fbdb59fde431e93ccf63
Jorge <jorgereina1986@gmail.com>
2018-11-07 14:12:27 -0500

    getting response and basic parsing

--- Weather.py
+++ Weather.py
@@ -1,7 +1,6 @@
 import requests
 import json
 
-# params=payload
 # zip=11237&apikey=0ba6294d35fd4d0ade6224193d18de15")
 payload = {"zip": "11237", "apikey": "0ba6294d35fd4d0ade6224193d18de15"}
 
@@ -9,7 +8,19 @@ weather_requests = requests.get("http://api.openweathermap.org/data/2.5/weather?
 
 json_root = weather_requests.text
 
-todo = json.loads(json_root)
-print(todo)
+json_string = json.loads(json_root)
+
+# weather temp
+main = json_string["main"]
+temp = main["temp"]
+coord = json_string["coord"]
+
+
+lat = coord["lat"]
+lon = coord["lon"]
+
+print("Temperature: {}".format(temp))
+print("Lattitude: {} \nLongitude: {}".format(lat, lon))
+
 print(json_root)
 
