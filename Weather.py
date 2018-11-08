cb4d64db4782166a8cb61ce5492a74fad712214d
eddymark <eddyv5413@gmail.com>
2018-11-08 15:24:55 -0500

    configured gui things with the weather app

--- Weather.py
+++ Weather.py
@@ -1,45 +1,11 @@
+from WeatherGui import GUI
+
 import requests
+
 import json
 
 # zip=11237&apikey=0ba6294d35fd4d0ade6224193d18de15")
-payload = {"zip": "11237", "units": "imperial", "apikey": "0ba6294d35fd4d0ade6224193d18de15"}
 
-weather_requests = requests.get("http://api.openweathermap.org/data/2.5/weather?", params=payload)
 
-json_root = weather_requests.text
 
-json_string = json.loads(json_root)
-
-# Weather temp
-main = json_string["main"]
-temp = int(main["temp"])
-
-# Max/Min temp
-temp_max = int(main["temp_max"])
-temp_min = int(main["temp_min"])
-
-# Humidity
-humidity = main["humidity"]
-
-# Description
-weather = json_string["weather"]
-weather_arr = weather[0]
-main_description = weather_arr["main"]
-
-# Coordinates
-coord = json_string["coord"]
-lat = coord["lat"]
-lon = coord["lon"]
-
-# City
-city = json_string["name"]
-
-print(city)
-print("Temperature: {}".format(temp))
-print("Humidity: {}%".format(humidity))
-print("Max/Min: {}/{}".format(temp_max, temp_min))
-print("Description: {}".format(main_description))
-print("Lattitude: {} \nLongitude: {}".format(lat, lon))
-
-print(json_root)
-
+GUI("New York")
--- WeatherGui.py
+++ WeatherGui.py
@@ -1,9 +1,39 @@
 from tkinter import *
+from WeatherRequest import WeatherRequest
 
-window = Tk()
+class GUI:
+    def __init__(self, title):
+        self.window = Tk()
+        self.window.title(title)
+        self.window.geometry("600x300+0+0")
+        self.request = WeatherRequest()
 
-Label(window, text="temperature").grid(row=0)
-Label(window, text="weatherValue").grid(row=1)
+        self.e1 = Entry(self.window)
+        self.e1.grid(row=0)
+        Label(self.window, text="weatherValue").grid(row=1)
 
+        button = Button(self.window, text="Print Me", command=self.getUserRequest)
+        escape = Button(self.window, text="Print Me", command=quit)
 
+        button.grid(row=3)
+        escape.grid(row=4)
 
+        self.window.mainloop()
+
+    # def printSomething(self):
+    #     # if you want the button to disappear:
+    #     # button.destroy() or button.pack_forget()
+    #     lab = Label(self.window, text=)
+    #     lab.grid(row=2)
+    #     #this creates a new label to the GUI
+
+    def getUserRequest(self):
+        zipcode = self.getZip()
+
+        # makes request
+        self.request.makeRequest(zipcode)
+
+        print(self.request.getData())
+
+    def getZip(self):
+        return self.e1.get()
